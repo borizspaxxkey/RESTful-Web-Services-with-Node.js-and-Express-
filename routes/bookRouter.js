87b57ef1f7d7aef4aa83f6b277f55eaa9da9c47c
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2019-11-19 07:46:24 -0600

    HATEOAS

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,39 +1,77 @@
+/* eslint-disable no-param-reassign */
 const express = require('express');
+const booksController = require('../controllers/booksController');
 
 function routes(Book) {
   const bookRouter = express.Router();
-
+  const controller = booksController(Book);
   bookRouter.route('/books')
-    .post((req, res) => {
-      const book = new Book(req.body);
+    .post(controller.post)  
+    .get(controller.get);
 
-      book.save();
-      return res.status(201).json(book);
-    })
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
       }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
     });
-
+  })
   bookRouter.route('/books/:bookId')
     .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
+      const returnBook = req.book.toJSON();
+
+      returnBook.links = {};
+      const genre = req.book.genre.replace(' ', '%20');
+      returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+      res.json(returnBook);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
         if (err) {
           return res.send(err);
         }
         return res.json(book);
       });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+
+      // eslint-disable-next-line no-underscore-dangle 
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
     });
 
-    return bookRouter;
+  return bookRouter;
 }
 
 module.exports = routes;
